89cef8a305e068cee076711632693380c523bbf2
Kamil Pochylski <kamilpo@interia.pl>
2023-03-10 20:05:04 +0100

    6 dzień Employee

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,72 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Metrics;
+﻿using ChallengeApp;
 
-int number = 1223334444;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
-int Counter0 = 0;
-int Counter1 = 0;
-int Counter2 = 0;
-int Counter3 = 0;
-int Counter4 = 0;
-int Counter5 = 0;
-int Counter6 = 0;
-int Counter7 = 0;
-int Counter8 = 0;
-int Counter9 = 0;
-foreach (char letter in letters)
+Employee employee1 = new Employee("Kamil", "Kowalski", 40);
+Employee employee2 = new Employee("Ania", "Daszek", 33);
+Employee employee3 = new Employee("Ewa", "Zielińska", 28);
+
+employee1.AddScore(5);
+employee1.AddScore(9);
+employee1.AddScore(9);
+employee1.AddScore(7);
+employee1.AddScore(6);
+
+employee2.AddScore(7);
+employee2.AddScore(3);
+employee2.AddScore(1);
+employee2.AddScore(5);
+employee2.AddScore(9);
+
+employee3.AddScore(6);
+employee3.AddScore(1);
+employee3.AddScore(3);
+employee3.AddScore(7);
+employee3.AddScore(5);
+
+
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == '0')
+
+    employee1, employee2, employee3
+
+};
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var Employee in employees)
+{
+    if (Employee.Result > maxResult)
     {
-        Counter0++;
+        maxResult = Employee.Result;
+        employeeWithMaxResult = Employee;
     }
-    else if (letter == '1')
-    {
-        Counter1++;
-    }
-    else if (letter == '2')
-    {
-        Counter2++;
-    }
-    else if (letter == '3')
-    {
-        Counter3++;
-    }
-    else if (letter == '4')
-    {
-        Counter4++;
-    }
-    else if (letter == '5')
-    {
-        Counter5++;
-    }
-    else if (letter == '6')
-    {
-        Counter6++;
-    }
-    else if (letter == '7')
-    {
-        Counter7++;
-    }
-    else if (letter == '8')
-    {
-        Counter8++;
-    }
-    else if (letter == '9')
-    {
-        Counter9++;
-    }
+
 }
 
-Console.WriteLine("0 =>" + Counter0);
-Console.WriteLine("1 =>" + Counter1);
-Console.WriteLine("2 =>" + Counter2);
-Console.WriteLine("3 =>" + Counter3);
-Console.WriteLine("4 =>" + Counter4);
-Console.WriteLine("5 =>" + Counter5);
-Console.WriteLine("6 =>" + Counter6);
-Console.WriteLine("7 =>" + Counter7);
-Console.WriteLine("8 =>" + Counter8);
-Console.WriteLine("9 =>" + Counter9);
+Console.WriteLine("Najlepszy pracownik"  + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " + maxResult + "punktów");
 
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        public List<int> score = new List<int>();
+        public Employee(string name, string surename, int age)
+
+        {
+            this.Name = name;
+            this.Surname = surename;
+            this.Age = age;
+
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return score.Sum();
+            }
+        }
+
+        public void AddScore(int score)
+        {
+
+            this.score.Add(score);
+        }
+
+    }
+}
