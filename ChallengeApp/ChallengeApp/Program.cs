9e3a51737ce31ae715ea48d2f3540029410c4b83
Kamil Pochylski <kamilpo@interia.pl>
2023-03-14 20:29:46 +0100

    7 dzień

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -4,15 +4,15 @@ Employee employee1 = new Employee("Kamil", "Kowalski", 40);
 Employee employee2 = new Employee("Ania", "Daszek", 33);
 Employee employee3 = new Employee("Ewa", "Zielińska", 28);
 
-employee1.AddScore(5);
 employee1.AddScore(9);
+employee1.AddScore(1);
 employee1.AddScore(9);
 employee1.AddScore(7);
 employee1.AddScore(6);
 
-employee2.AddScore(7);
-employee2.AddScore(3);
-employee2.AddScore(1);
+employee2.AddScore(-9);
+employee2.AddScore(-9);
+employee2.AddScore(9);
 employee2.AddScore(5);
 employee2.AddScore(9);
 
