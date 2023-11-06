a66b44aa5d0a21697484edda416866e918e6301e
Kamil Pochylski <kamilpo@interia.pl>
2023-11-06 19:35:18 +0100

    4/21

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic.FileIO;
+
+string name = "Ewa";
+var sex = "male";
+int age = 15;
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełonoletni mężczyzna");
+}
+
